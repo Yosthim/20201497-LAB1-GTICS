edd2e09d7ea18da424f6c8ae23eb97e6ad5d1ef4
Yosthim <a20201497@pucp.edu.pe>
2024-04-04 20:45:00 -0500

    Proyecto Sping boot creado en Intelli

---
+++ LAB1_GTICS_20201497/src/main/java/com/example/lab1_gtics_20201497/Lab1Gtics20201497Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20201497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Gtics20201497Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Gtics20201497Application.class, args);
+    }
+
+}
---
+++ LAB1_GTICS_20201497/src/test/java/com/example/lab1_gtics_20201497/Lab1Gtics20201497ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20201497;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Gtics20201497ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
